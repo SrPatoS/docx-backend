def9062cf4def3d95913e27415a7338779ac2833
Vinicius <vavizvini000@gmail.com>
2025-03-03 18:00:02 -0300

    adiciona model de relatório de trabalho

---
+++ src/models/work-report.model.ts
@@ -0,0 +1,55 @@
+﻿import { model, Schema } from "mongoose";
+import { IModel, modelConfig, modelKeysDefault } from "@src/models/utils/model.utils";
+
+export interface IReport {
+  date: Date;
+  observation: string;
+}
+
+export interface IWorkReport extends IModel {
+  userName: string;
+  userId: Id;
+  companyId: Id;
+  companyName: string;
+  startWork: IReport;
+  endWork: IReport;
+  startLunch: IReport;
+  endLunch: IReport;
+}
+
+const reportSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  observation: {
+    type: String,
+    required: false,
+  }
+});
+
+export const workReportModel = model<IWorkReport>("workReportModel", new Schema({
+  ...modelKeysDefault,
+  userName: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  companyId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: false,
+  },
+  companyName: {
+    type: String,
+    required: false,
+  },
+  startWork: reportSchema,
+  endWork: reportSchema,
+  startLunch: reportSchema,
+  endLunch: reportSchema,
+}, modelConfig), "work_report");
