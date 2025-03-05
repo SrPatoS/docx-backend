8ba146d217b3d6a8ad01646966d58590a94ff532
antony-souza <antonygustavo09@hotmail.com>
2025-03-05 11:47:27 -0300

    Resolvendo merge

--- src/api/module/company-api/company-api.schema.ts
+++ src/api/module/company-api/company-api.schema.ts
@@ -1,20 +1,8 @@
 import { z } from "zod";
 
-const userApiSchema = z.array(z.object({
-    name: z.string(),
-    _id: z.string(),
-}));
-
 export const companyApiSchema = z.object({
-<<<<<<< HEAD
-    avatar: z.string().optional(),
-    name: z.string(),
-    cnpj: z.string().min(18),
-    users: userApiSchema.optional()
-=======
 	avatar: z.string().optional(),
 	name: z.string(),
 	cnpj: z.string().min(18),
 	uniqueCode: z.string().min(1)
->>>>>>> main
 });
--- src/api/module/company-api/usecases/company-api.usecase.service.ts
+++
@@ -1,9 +0,0 @@
-import { ApiResponse } from "@src/api/_types/api-response.type";
-import { companyModel, ICompany } from "@src/models/company.model";
-
-export class CompanyApiService {
-    async handler(companyData: ICompany): Promise<ApiResponse> {
-        
-        return { message: "Company created successfully.", errors: [] };
-    }
-}
