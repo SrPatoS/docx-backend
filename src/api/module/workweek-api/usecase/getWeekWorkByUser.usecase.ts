027a64e24946ab12603692b8f8d04373a4023af9
antony-souza <antonygustavo09@hotmail.com>
2025-03-12 15:15:55 -0300

    Retornando carga horária do funcionário autenticado

--- src/api/module/workweek-api/usecase/getWeekWorkByUser.usecase.ts
+++ src/api/module/workweek-api/usecase/getWeekWorkByUser.usecase.ts
@@ -3,8 +3,48 @@ import { workWeekModel } from "@src/models/work-week.model";
 
 export class GetWeekWorkByUserUseCase {
     async handle(userId: string): Promise<ApiResponse> {
-        const workWeek = await workWeekModel.findOne({ userId: userId })
-        .select("-__v -createdAt -updatedAt")
+
+        const days = ["segunda", "terca", "quarta", "quinta", "sexta", "sabado", "domingo"];
+
+        const dataProject: Record<string, any> = { _id: 0 };
+
+        days.forEach((day) => {
+            dataProject[day] = {
+                start: {
+                    $dateToString: {
+                        format: "%H:%M",
+                        date: `$${day}.start`,
+                        timezone: "America/Sao_Paulo",
+                    },
+                },
+                lunchStart: {
+                    $dateToString: {
+                        format: "%H:%M",
+                        date: `$${day}.lunchStart`,
+                        timezone: "America/Sao_Paulo",
+                    },
+                },
+                lunchEnd: {
+                    $dateToString: {
+                        format: "%H:%M",
+                        date: `$${day}.lunchEnd`,
+                        timezone: "America/Sao_Paulo",
+                    },
+                },
+                end: {
+                    $dateToString: {
+                        format: "%H:%M",
+                        date: `$${day}.end`,
+                        timezone: "America/Sao_Paulo",
+                    },
+                },
+            };
+        });
+
+        const workWeek = await workWeekModel.aggregate([
+            { $match: { userId: userId } },
+            { $project: dataProject },
+        ]);
 
         if (!workWeek) {
             return {
