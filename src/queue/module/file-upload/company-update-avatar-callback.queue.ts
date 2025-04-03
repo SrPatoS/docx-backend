e3d43445800a198b940057a0e4cd15ceffa60b7f
Vinicius <vavizvini000@gmail.com>
2025-04-02 23:12:33 -0300

    a

--- src/queue/module/file-upload/company-update-avatar-callback.queue.ts
+++ src/queue/module/file-upload/company-update-avatar-callback.queue.ts
@@ -5,6 +5,7 @@ import { MongoUtils } from "@src/api/_utils/mongo.utils";
 export class CompanyUpdateAvatarCallbackQueue implements RabbitmqQueue {
 	name: QueueName = "company-avatar-callback";
 
+
 	async handler(data: {
 		url: string;
 		other: string;
